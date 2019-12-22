51dd2c8aa0e21f874755cac49db4e292cc2ae167
cake <larosteguy@gmail.com>
2019-12-22 14:46:49 -0700

    date added to header

---
+++ script.js
@@ -0,0 +1,17 @@
+
+// date in jumbotron
+
+
+var c = moment().format("LLLL");
+         document.getElementById("currentDay").innerHTML = c;
+
+
+
+
+
+
+// connect times to clock
+// past time-frame rows
+//present time row
+//future time row
+// save button connected to local storage
