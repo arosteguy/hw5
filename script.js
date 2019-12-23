4811181a774139fdeb5124f6a89759cb0cfa5848
cake <larosteguy@gmail.com>
2019-12-23 12:20:59 -0700

    work on local storage

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 
 // date in jumbotron
+var task = localStorage.getItem("task");
+var saveBtn = document.querySelectorAll("#saveBtn");
+var a = moment.locale("America, Denver");
 
-var a = moment.locale("us");
+// connect times to clock
 var c = moment().format("LL");
          document.getElementById("currentDay").innerHTML = c;
 
- // connect times to clock
-
- var t = new Date().getHours();
+render
+//  var t = new Date().getHours();
 
  //if (t < 10) {
 //      document.write("Have a good morning!");
@@ -17,13 +19,13 @@ var c = moment().format("LL");
 //      document.write("Have a good night!");
 //  }
 
- if (t < now) {
-    document.getElementById("class", ".past");
-} else if (t < 20) {
-    document.write("Have a good day!");
-} else {
-    document.write("Have a good night!");
-}
+ //if (t < now) {
+  //  document.getElementById("class", ".past");
+//} else if (t < 20) {
+ //   document.write("Have a good day!");
+//} else {
+ //   document.write("Have a good night!");
+//}
 
 //ref to form
 //ref to past time-frame rows
@@ -33,6 +35,13 @@ var c = moment().format("LL");
 //clear the input?
 
 
-
+// function render task
+function render
 
 // save button connected to local storage   (submit event)
+  saveBtn.addEventListener("click", function (event){
+    event.preventDefault();
+    
+     var nine = documentQuerySelector("#nine").value;
+    localStorage.setItem("#nine", nine);
+})
