e817b90e7b55d7b21ff1b73b3471828e4902e44d
cake <larosteguy@gmail.com>
2019-12-22 17:12:00 -0700

    work on time and local storage

--- script.js
+++ script.js
@@ -1,17 +1,38 @@
 
 // date in jumbotron
 
-
-var c = moment().format("LLLL");
+var a = moment.locale("us");
+var c = moment().format("LL");
          document.getElementById("currentDay").innerHTML = c;
 
+ // connect times to clock
+
+ var t = new Date().getHours();
+
+ //if (t < 10) {
+//      document.write("Have a good morning!");
+//  } else if (t < 20) {
+//      document.write("Have a good day!");
+//  } else {
+//      document.write("Have a good night!");
+//  }
+
+ if (t < now) {
+    document.getElementById("class", ".past");
+} else if (t < 20) {
+    document.write("Have a good day!");
+} else {
+    document.write("Have a good night!");
+}
+
+//ref to form
+//ref to past time-frame rows
+//ref to present time row
+//ref to future time row
+//user input stored in var
+//clear the input?
 
 
 
 
-
-// connect times to clock
-// past time-frame rows
-//present time row
-//future time row
-// save button connected to local storage
+// save button connected to local storage   (submit event)
